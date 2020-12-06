0ec082defd91ef56c9a9b4c973c0edb3014c356d
Punam Nandi <punamnandi@Punams-Air.Dlink>
2020-12-07 01:07:21 +0530

    Endpoint implemented to fetch all the questions and edit question content.

--- Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ Trello_quora/quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -4,11 +4,15 @@ import com.upgrad.quora.api.model.*;
 import com.upgrad.quora.service.business.QuestionService;
 import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -25,9 +29,7 @@ public class QuestionController {
      * @throws AuthorizationFailedException In case the access token is invalid.
      */
     @RequestMapping(method = RequestMethod.POST, path = "/question/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<QuestionResponse> createQuestion(
-            @RequestHeader("authorization") final String accessToken, QuestionRequest questionRequest)
-            throws AuthorizationFailedException {
+    public ResponseEntity<QuestionResponse> createQuestion(@RequestHeader("authorization") final String accessToken, QuestionRequest questionRequest) throws AuthorizationFailedException {
         QuestionEntity questionEntity = new QuestionEntity();
         questionEntity.setContent(questionRequest.getContent());
         questionEntity = questionService.createQuestion(questionEntity, accessToken);
@@ -35,6 +37,53 @@ public class QuestionController {
         questionResponse.setId(questionEntity.getUuid());
         questionResponse.setStatus("QUESTION CREATED");
         return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
+    }
+
+    /**
+     * Get all questions posted by any user.
+     * @param accessToken access token to authenticate user.
+     * @return List of QuestionDetailsResponse
+     * @throws AuthorizationFailedException In case the access token is invalid.
+     */
+    @RequestMapping(
+            method = RequestMethod.GET, path = "/question/all", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestions(@RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException {
+        List<QuestionEntity> questions = questionService.getAllQuestions(accessToken);
+        List<QuestionDetailsResponse> questionDetailResponses = new ArrayList<>();
+        for (QuestionEntity questionEntity : questions) {
+            QuestionDetailsResponse questionDetailResponse = new QuestionDetailsResponse();
+            questionDetailResponse.setId(questionEntity.getUuid());
+            questionDetailResponse.setContent(questionEntity.getContent());
+            questionDetailResponses.add(questionDetailResponse);
+        }
+        return new ResponseEntity<List<QuestionDetailsResponse>>(
+                questionDetailResponses, HttpStatus.OK);
+    }
+
+    /**
+     * Edit a question
+     * @param accessToken access token to authenticate user.
+     * @param questionId id of the question to be edited.
+     * @param questionEditRequest new content for the question.
+     * @return Id and status of the question edited.
+     * @throws AuthorizationFailedException In case the access token is invalid.
+     * @throws InvalidQuestionException if question with questionId doesn't exist.
+     */
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            path = "/question/edit/{questionId}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<QuestionEditResponse> editQuestion(
+            @RequestHeader("authorization") final String accessToken,
+            @PathVariable("questionId") final String questionId,
+            QuestionEditRequest questionEditRequest)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        QuestionEntity questionEntity =
+                questionService.editQuestion(accessToken, questionId, questionEditRequest.getContent());
+        QuestionEditResponse questionEditResponse = new QuestionEditResponse();
+        questionEditResponse.setId(questionEntity.getUuid());
+        questionEditResponse.setStatus("QUESTION EDITED");
+        return new ResponseEntity<QuestionEditResponse>(questionEditResponse, HttpStatus.OK);
     }
 
 
